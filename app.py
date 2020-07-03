ec493a386922ce6e3ab2ad1c63f8c7b8eebdf775
Ari Juanda <ari@pop-os.localdomain>
2020-07-03 22:01:51 +0700

    update

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import os
 from flask import Flask
+
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTING'])
 
 
 @app.route('/')
@@ -12,6 +16,7 @@ if __name__ == '__main__':
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
 
 if __name__ == '__main__':
     app.run()
